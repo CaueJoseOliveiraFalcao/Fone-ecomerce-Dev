91feadf8f4fef2d4dd66fb85560f3a9ada9e6374
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2022-02-17 15:10:51 -0400

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+let menu_mobile = document.getElementById('mobile-nav')
+let botao_mobile = document.getElementById('botao_m')
+
+function abrir_menu(){
+    menu_mobile.style.display = 'block'
+    console.log('foi')
+}
+
+function fechar(){
+    menu_mobile.style.display = 'none'
+}
